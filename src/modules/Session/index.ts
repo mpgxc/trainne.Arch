04d3e6fe023ad867fed6202d58cd779e8b8decfc
Mateus Garcia <mpgx5.c@gmail.com>
2021-01-04 15:37:53 -0300

    feat: Criação de factory para inversão de depedência do express

--- src/modules/Session/index.ts
+++ src/modules/Session/index.ts
@@ -1,1 +1,12 @@
-export { default as SessionRouter } from './SessionRoutes';
+// export { default as SessionRouter } from './SessionRoutes';
+import SessionInstanceFactory from './SessionInstanceFactory';
+import { useRouter } from './SessionRouter';
+import { IUseRouter } from '../../@types/IUseRouter';
+
+const useSessionRouter = (appRouter: IUseRouter): void => {
+  new useRouter({
+    appRouter,
+    SessionModule: SessionInstanceFactory.getInstance(),
+  });
+};
+export { useSessionRouter };
